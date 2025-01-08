635489006862fac5c3c5cb4a9257d1c0aff2dcf5
alphaom33 <alphaom34@gmail.com>
2025-01-07 20:11:23 -0600

    gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "efccf258b02ffa3dc8a4aa8b05dfcfd3316932f2";
-  public static final String GIT_DATE = "2025-01-07 20:51:34 EST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "374b985880bfea6213e796238401d330981e480b";
+  public static final String GIT_DATE = "2025-01-07 21:07:11 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-07 21:01:06 EST";
-  public static final long BUILD_UNIX_TIME = 1736301666328L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-07 21:09:47 EST";
+  public static final long BUILD_UNIX_TIME = 1736302187190L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }

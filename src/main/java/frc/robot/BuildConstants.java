5cc42daec6b68dd2f543a8a6c8a25399cd5f1fe8
alphaom33 <alphaom34@gmail.com>
2025-01-11 13:43:19 -0600

    profiling; it doesn't steal all my cpu

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "440c7fa9d9a7634712e51b21eda0f52f8b6a17a6";
-  public static final String GIT_DATE = "2025-01-11 12:09:53 EST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "08be432cb0ac87d281acb974e440e94e8c16f867";
+  public static final String GIT_DATE = "2025-01-11 12:18:40 EST";
   public static final String GIT_BRANCH = "disabledPeriodic";
-  public static final String BUILD_DATE = "2025-01-11 12:16:23 EST";
-  public static final long BUILD_UNIX_TIME = 1736615783241L;
+  public static final String BUILD_DATE = "2025-01-11 12:34:21 EST";
+  public static final long BUILD_UNIX_TIME = 1736616861416L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

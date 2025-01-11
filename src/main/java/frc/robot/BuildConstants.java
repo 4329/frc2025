c1b2d1588665514752dc5526231a699eccdbe0fe
alphaom33 <alphaom34@gmail.com>
2025-01-11 11:18:40 -0600

    works

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "5dad4a143e1058ab846424a1a2a5a0e316ef3409";
-  public static final String GIT_DATE = "2025-01-10 21:19:29 EST";
-  public static final String GIT_BRANCH = "swerveSim";
-  public static final String BUILD_DATE = "2025-01-11 11:11:08 EST";
-  public static final long BUILD_UNIX_TIME = 1736611868684L;
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "440c7fa9d9a7634712e51b21eda0f52f8b6a17a6";
+  public static final String GIT_DATE = "2025-01-11 12:09:53 EST";
+  public static final String GIT_BRANCH = "disabledPeriodic";
+  public static final String BUILD_DATE = "2025-01-11 12:16:23 EST";
+  public static final long BUILD_UNIX_TIME = 1736615783241L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

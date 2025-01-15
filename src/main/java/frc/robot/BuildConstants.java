411e1c478a69f19f5543abdf3c38732d833ba8a6
Cameron Bell <cameron.bell@lhssc.org>
2025-01-14 20:29:42 -0600

    Fixed Drivetrain. Modified turning encoder settings, and added redux lib

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "c732c795e31a84694824607f831abe1928e7d55e";
-  public static final String GIT_DATE = "2025-01-14 21:29:42 EST";
-  public static final String GIT_BRANCH = "betterDriving";
-  public static final String BUILD_DATE = "2025-01-15 20:07:46 EST";
-  public static final long BUILD_UNIX_TIME = 1736989666223L;
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "255fd1bab4fd4f41029c4ca342d4afb54e1c707c";
+  public static final String GIT_DATE = "2025-01-14 19:53:39 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-14 21:17:54 EST";
+  public static final long BUILD_UNIX_TIME = 1736907474456L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

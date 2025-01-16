630b1404d8783f95afd28e4be99733c39f7c8405
alphaom33 <alphaom34@gmail.com>
2025-01-15 20:20:52 -0600

    maybe

--- src/main/java/frc/robot/subsystems/swerve/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/swerve/drivetrain/Drivetrain.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.swerve.drivetrain;
 
+import com.pathplanner.lib.util.DriveFeedforwards;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
--- src/main/java/frc/robot/subsystems/swerve/drivetrain/DrivetrainImpl.java
+++ src/main/java/frc/robot/subsystems/swerve/drivetrain/DrivetrainImpl.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.swerve.drivetrain;
 
+import com.pathplanner.lib.util.DriveFeedforwards;
 import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 import edu.wpi.first.math.controller.PIDController;

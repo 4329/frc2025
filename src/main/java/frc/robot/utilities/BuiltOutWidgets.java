f05ebd6043a266be0eec6f357c2e06b9c9715683
alphaom33 <alphaom34@gmail.com>
2025-01-25 14:25:12 -0600

    yep

--- src/main/java/frc/robot/utilities/BuiltOutWidgets.java
+++ src/main/java/frc/robot/utilities/BuiltOutWidgets.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj.shuffleboard.WidgetType;
 public enum BuiltOutWidgets implements WidgetType {
   kRadiableGyro("RadiableGyro"),
   kSometimesText("Sometimes Text"),
-  kLoadBar("Loading Bar");
+  kLoadBar("Loading Bar"),
+  kBetterPIDController("BetterPID Controller");
 
   public String string;
 

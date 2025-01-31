681cec9a92ae80ac3cd4c4e35a95231387cb17e6
alphaom33 <alphaom34@gmail.com>
2025-01-30 18:56:48 -0600

    deadband fix thing

--- src/main/java/frc/robot/utilities/MathUtils.java
+++ src/main/java/frc/robot/utilities/MathUtils.java
@@ -49,13 +49,16 @@ public class MathUtils {
    * @return the resulting input after applying the deadband
    */
   public static double applyDeadband(double input) {
-    if (Math.abs(input) < DriveConstants.kInnerDeadband) {
-      return 0.0;
-    } else if (Math.abs(input) > DriveConstants.kOuterDeadband) {
-      return Math.signum(input) * 1.0;
-    } else {
-      return input;
-    }
+    double tmput;
+    if (Math.abs(input) < DriveConstants.kInnerDeadband - DriveConstants.kLowerBound) tmput = 0;
+    else if (Math.abs(input) > DriveConstants.kOuterDeadband) tmput = 1;
+    else
+      tmput =
+          Math.abs(
+              (input + DriveConstants.kInnerDeadband)
+                  * (DriveConstants.kOuterDeadband - DriveConstants.kInnerDeadband));
+
+    return Math.signum(input) * tmput;
   }
 
   /**

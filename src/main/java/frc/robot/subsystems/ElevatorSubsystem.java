187069ec21db1521de354ccc70dab0c1ae96e308
Cameron Bell <cameron.bell@lhssc.org>
2025-02-20 20:08:08 -0600

    set elevator position conversion factor and limits.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,6 +5,7 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SoftLimitConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,15 +18,17 @@ import org.littletonrobotics.junction.inputs.LoggableInputs;
 public class ElevatorSubsystem extends SubsystemBase implements LoggedSubsystem {
     private final double ELEVATOR_SPEED = .5;
 
-    private final double MIN = 0;
-    private final double MAX = 100;
+    private final double MIN = -22.589;
+    private final double MAX = 58.91;
 
     private final double MAX_INPUT_CONSTANT_K = 0.4329;
 
     public enum ElevatorPosition {
-        LOW(0),
-        MIDDLE(50),
-        HIGH(100);
+        L2(-1.778),
+        L3(13.972),
+        L4(38.347),
+        MAX_HEIGHT(58.91),
+        ;
 
         double pos;
 
@@ -46,19 +49,26 @@ public class ElevatorSubsystem extends SubsystemBase implements LoggedSubsystem 
         motor1 = SparkFactory.createSparkMax(10);
         motor2 = SparkFactory.createSparkMax(12);
 
-        motor1.configure(
+        SparkBaseConfig configgled =
                 new SparkMaxConfig()
                         .apply(
                                 new SoftLimitConfig()
                                         .forwardSoftLimit(MAX)
                                         .forwardSoftLimitEnabled(true)
                                         .reverseSoftLimit(MIN)
-                                        .reverseSoftLimitEnabled(true)),
-                ResetMode.kResetSafeParameters,
-                PersistMode.kPersistParameters);
+                                        .reverseSoftLimitEnabled(true));
+
+        final double gear1 = 11;
+        final double gear2 = 54;
+        final double gear3 = 28;
+        final double belt = 5;
+        configgled.encoder.positionConversionFactor(
+                (gear1 / gear2) * (gear2 / gear3) * (gear3 / belt)); // approximation
+
+        motor1.configure(configgled, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         motor2.configure(
-                new SparkMaxConfig().follow(motor1, true).inverted(true),
+                new SparkMaxConfig().follow(motor1, true),
                 ResetMode.kNoResetSafeParameters,
                 PersistMode.kPersistParameters);
 

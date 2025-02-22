73d1b1358564ae83e19997d021ef27273ed89e74
alphaom33 <alphaom34@gmail.com>
2025-02-22 14:57:32 -0600

    started simmy

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSim.java
@@ -3,21 +3,42 @@ package frc.robot.subsystems.elevator;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.inputs.LoggableInputs;
-import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
-import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
 
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import com.revrobotics.sim.SparkMaxSim;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 
 public class ElevatorSim extends ElevatorImpl {
-	@AutoLogOutput
-	private LoggedMechanism2d mechanism;
+
+	private SparkMaxSim motorSim;
+	private edu.wpi.first.wpilibj.simulation.ElevatorSim elevatorSim;
+
 
 	public ElevatorSim() {
 		super();
 
-		mechanism = new LoggedMechanism2d(3, 3);
-		mechanism.getRoot("root", 1.5, 0).append(new LoggedMechanismLigament2d("thing", 0.9, 90));
+		DCMotor gearbox = DCMotor.getNEO(1);
+		motorSim = new SparkMaxSim(motor1, gearbox);
+		elevatorSim = new edu.wpi.first.wpilibj.simulation.ElevatorSim(
+				gearbox,
+				1,
+				100,
+				Units.inchesToMeters(1.788 / 2),
+				0,
+				100,
+				true,
+				0,
+				0.01,
+				0.0);
+
 	}
 
+	@Override
+	public void simulationPeriodic() {
+	}
 }

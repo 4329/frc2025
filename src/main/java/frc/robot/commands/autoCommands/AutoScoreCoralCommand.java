5dc9272b893ed96a535873a9b928dce01f710877
RiceOverdose <johngong137@gmail.com>
2025-02-27 19:56:55 -0600

    maybe this is fine now?

---
+++ src/main/java/frc/robot/commands/autoCommands/AutoScoreCoralCommand.java
@@ -0,0 +1,45 @@
+package frc.robot.commands.autoCommands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.algeePivotCommands.SetAlgeePivotCommand;
+import frc.robot.commands.differentialArmCommands.SetArmPitchCommand;
+import frc.robot.subsystems.AlgeePivotSubsystem;
+import frc.robot.subsystems.AlgeePivotSubsystem.AlgeePivotAngle;
+import frc.robot.subsystems.PoseEstimationSubsystem;
+import frc.robot.subsystems.differentialArm.DifferentialArmSubsystem;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.elevator.ElevatorSubsystem.ElevatorPosition;
+import frc.robot.subsystems.swerve.drivetrain.Drivetrain;
+import frc.robot.utilities.NotFinalSequentialCommandGroup;
+import frc.robot.utilities.UnInstantCommand;
+
+public class AutoScoreCoralCommand extends NotFinalSequentialCommandGroup {
+
+    public AutoScoreCoralCommand(
+            AlgeePivotSubsystem algeePivotSubsystem,
+            ElevatorSubsystem elevatorSubsystem,
+            ElevatorPosition elevatorPosition,
+            DifferentialArmSubsystem differentialArmSubsystem,
+            PoseEstimationSubsystem poseEstimationSubsystem,
+            Drivetrain drivetrain) {
+
+        addCommands(
+                new SetAlgeePivotCommand(algeePivotSubsystem, AlgeePivotAngle.ZERO),
+                new ParallelCommandGroup(
+                        new UnInstantCommand(
+                                        () ->
+                                                elevatorSubsystem.setSetpoint(
+                                                        switch (elevatorPosition) {
+                                                            case L2 -> ElevatorSubsystem.ElevatorPosition.L2;
+                                                            case L3 -> ElevatorSubsystem.ElevatorPosition.L3;
+                                                            case L4 -> ElevatorSubsystem.ElevatorPosition.L4;
+                                                            default -> ElevatorSubsystem.ElevatorPosition.L2;
+                                                        }))
+                                .until(() -> elevatorSubsystem.atSetpoint()),
+                        new SetArmPitchCommand(
+                                differentialArmSubsystem,
+                                DifferentialArmSubsystem.DifferentialArmPitch.ONETHIRTYFIVE)),
+                new SetArmPitchCommand(
+                        differentialArmSubsystem, DifferentialArmSubsystem.DifferentialArmPitch.NINETY));
+    }
+}

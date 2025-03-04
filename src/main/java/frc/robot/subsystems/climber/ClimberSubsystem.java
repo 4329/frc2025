f63b2c17ea136eaeed964aa5160ce7a37582f68f
Samcmyk <sjaworski717@gmail.com>
2025-03-03 18:46:05 -0600

    Spotless

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,31 +1,28 @@
 package frc.robot.subsystems.climber;
 
-import org.littletonrobotics.junction.inputs.LoggableInputs;
-
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SoftLimitConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.model.ClimberLogAutoLogged;
 import frc.robot.subsystems.LoggingSubsystem.LoggedSubsystem;
 import frc.robot.utilities.MathUtils;
 import frc.robot.utilities.SparkFactory;
 import frc.robot.utilities.shufflebored.ShuffledPIDController;
-import frc.robot.Constants;
-import frc.robot.model.ClimberLogAutoLogged;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 public class ClimberSubsystem extends SubsystemBase implements LoggedSubsystem {
-    private final double CLIMBY_SPEEDY = 0.5; //WIP
+    private final double CLIMBY_SPEEDY = 0.5; // WIP
 
     private final double ZERO = 0;
-    private final double MIN = -99999; //WIP
-    private final double MAX = 99999; //WIP
+    private final double MIN = -99999; // WIP
+    private final double MAX = 99999; // WIP
 
     private double setpoint = 0;
 
@@ -40,10 +37,17 @@ public class ClimberSubsystem extends SubsystemBase implements LoggedSubsystem {
     public ClimberSubsystem() {
         motor = SparkFactory.createSparkMax(Constants.SparkIDs.climber);
         encoder = motor.getEncoder();
-        config = new SparkMaxConfig().apply(new SoftLimitConfig().forwardSoftLimit(MAX).forwardSoftLimitEnabled(true).reverseSoftLimit(MIN).reverseSoftLimitEnabled(true));
+        config =
+                new SparkMaxConfig()
+                        .apply(
+                                new SoftLimitConfig()
+                                        .forwardSoftLimit(MAX)
+                                        .forwardSoftLimitEnabled(true)
+                                        .reverseSoftLimit(MIN)
+                                        .reverseSoftLimitEnabled(true));
         motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
 
-        pidController = new ShuffledPIDController(1, 0, 0); //WIP
+        pidController = new ShuffledPIDController(1, 0, 0); // WIP
 
         climberLogAutoLogged = new ClimberLogAutoLogged();
     }
@@ -71,5 +75,4 @@ public class ClimberSubsystem extends SubsystemBase implements LoggedSubsystem {
         climberLogAutoLogged.position = encoder.getPosition();
         return climberLogAutoLogged;
     }
-
 }

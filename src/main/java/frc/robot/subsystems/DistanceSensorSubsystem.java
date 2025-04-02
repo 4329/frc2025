f8c7980295e50181cb42a017438b4cf84a269d1e
Cameron Bell <cameron.bell@lhssc.org>
2025-04-01 21:20:54 -0500

    Pico sensor

--- src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.PicoColorSensor;
+
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
@@ -14,13 +16,19 @@ public class DistanceSensorSubsystem extends SubsystemBase {
         double distance;
     }
 
-    ColorSensorV3 distanceSensor = new ColorSensorV3(Port.kMXP);
+    PicoColorSensor distanceSensor = new PicoColorSensor();
     DistanceSensorLogAutoLogged distanceSensorLogAutoLogged = new DistanceSensorLogAutoLogged();
 
     public boolean getCoraled() {
-        distanceSensorLogAutoLogged.distance = distanceSensor.getProximity();
+        distanceSensorLogAutoLogged.distance = distanceSensor.getProximity0();
         Logger.processInputs("DistanceSensorSubsystem", distanceSensorLogAutoLogged);
 
         return distanceSensorLogAutoLogged.distance >= CORAL_AMOUNT;
+    }
+
+    @Override
+    public void periodic() {
+        Logger.recordOutput("IsSensorConnected", distanceSensor.isSensor0Connected());
+        getCoraled();
     }
 }

60ba9f287ae9ae44896d48d76efb782f7c502fda
spotless <spotless@github.com>
2025-04-05 18:15:16 +0000

    spotlessed

--- src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
@@ -1,12 +1,7 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.ColorSensorV3;
-
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.utilities.PicoColorSensor;
-
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
@@ -16,16 +11,16 @@ public class DistanceSensorSubsystem extends SubsystemBase {
         boolean coraled;
     }
 
-	DistanceSensorLogAutoLogged distanceSensorLogAutoLogged = new DistanceSensorLogAutoLogged();
-	DigitalInput pico = new DigitalInput(0);
+    DistanceSensorLogAutoLogged distanceSensorLogAutoLogged = new DistanceSensorLogAutoLogged();
+    DigitalInput pico = new DigitalInput(0);
 
     public boolean getCoraled() {
         return pico.get();
     }
 
-	@Override
-	public void periodic() {
-		distanceSensorLogAutoLogged.coraled = pico.get();
+    @Override
+    public void periodic() {
+        distanceSensorLogAutoLogged.coraled = pico.get();
         Logger.processInputs("DistanceSensorSubsystem", distanceSensorLogAutoLogged);
-	}
+    }
 }

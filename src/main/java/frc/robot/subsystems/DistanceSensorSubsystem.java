0923645629abe6581c865630cebc5349fa784827
MBorchert <95452187+MBorchert@users.noreply.github.com>
2025-04-05 13:54:19 -0500

    picoSensor (#41)
    * Pico sensor
    
    * dio
    
    * spotlessed
    
    ---------
    
    Co-authored-by: Cameron Bell <cameron.bell@lhssc.org>
    Co-authored-by: spotless <spotless@github.com>

--- src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
@@ -1,26 +1,26 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
 public class DistanceSensorSubsystem extends SubsystemBase {
-    private static final double CORAL_AMOUNT = 175;
-
     @AutoLog
     public static class DistanceSensorLog {
-        double distance;
+        boolean coraled;
     }
 
-    ColorSensorV3 distanceSensor = new ColorSensorV3(Port.kMXP);
     DistanceSensorLogAutoLogged distanceSensorLogAutoLogged = new DistanceSensorLogAutoLogged();
+    DigitalInput pico = new DigitalInput(0);
 
     public boolean getCoraled() {
-        distanceSensorLogAutoLogged.distance = distanceSensor.getProximity();
-        Logger.processInputs("DistanceSensorSubsystem", distanceSensorLogAutoLogged);
+        return pico.get();
+    }
 
-        return distanceSensorLogAutoLogged.distance >= CORAL_AMOUNT;
+    @Override
+    public void periodic() {
+        distanceSensorLogAutoLogged.coraled = pico.get();
+        Logger.processInputs("DistanceSensorSubsystem", distanceSensorLogAutoLogged);
     }
 }

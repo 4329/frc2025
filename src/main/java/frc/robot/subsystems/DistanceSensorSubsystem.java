3672e09c434e09599c39d75b447aed8fb2c1aadf
MBorchert <matthew.borchert@lhssc.org>
2025-04-05 12:56:35 -0500

    dio

--- src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utilities.PicoColorSensor;
@@ -9,26 +11,21 @@ import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
 public class DistanceSensorSubsystem extends SubsystemBase {
-    private static final double CORAL_AMOUNT = 175;
-
     @AutoLog
     public static class DistanceSensorLog {
-        double distance;
+        boolean coraled;
     }
 
-    PicoColorSensor distanceSensor = new PicoColorSensor();
-    DistanceSensorLogAutoLogged distanceSensorLogAutoLogged = new DistanceSensorLogAutoLogged();
+	DistanceSensorLogAutoLogged distanceSensorLogAutoLogged = new DistanceSensorLogAutoLogged();
+	DigitalInput pico = new DigitalInput(0);
 
     public boolean getCoraled() {
-        distanceSensorLogAutoLogged.distance = distanceSensor.getProximity0();
+        return pico.get();
+    }
+
+	@Override
+	public void periodic() {
+		distanceSensorLogAutoLogged.coraled = pico.get();
         Logger.processInputs("DistanceSensorSubsystem", distanceSensorLogAutoLogged);
-
-        return distanceSensorLogAutoLogged.distance >= CORAL_AMOUNT;
-    }
-
-    @Override
-    public void periodic() {
-        Logger.recordOutput("IsSensorConnected", distanceSensor.isSensor0Connected());
-        getCoraled();
-    }
+	}
 }
